d5c03d265b2f5ff48ee7d4aa0cf543b3f100de0c
Tarik <baba.tarik.git@gmail.com>
2024-08-27 13:10:03 +0200

    udpate team members

--- assets/script.js
+++ assets/script.js
@@ -1,7 +1,27 @@
-document.addEventListener('DOMContentLoaded', function () {
-   const toggleButton = document.getElementById('switch-mode');
-   toggleButton.addEventListener('click', function () {
-      document.body.classList.toggle('light-mode');
-      document.body.classList.toggle('dark-mode');
+document.addEventListener("DOMContentLoaded", function () {
+   const toggleButton = document.getElementById("switch-mode");
+   toggleButton.addEventListener("click", function () {
+      document.body.classList.toggle("light-mode");
+      document.body.classList.toggle("dark-mode");
+   });
+
+   const bioButtons = document.querySelectorAll(".btn-bio");
+
+   bioButtons.forEach((button) => {
+      const card = button.closest(".card");
+      const bioDiv = card.querySelector(".bio");
+      const cardInfo = card.querySelector(".profil");
+
+      bioDiv.style.display = "none";
+
+      button.addEventListener("click", function () {
+         if (bioDiv.style.display === "none" || bioDiv.style.display === "") {
+            bioDiv.style.display = "flex";
+            cardInfo.style.display = "none";
+         } else {
+            bioDiv.style.display = "none";
+            cardInfo.style.display = "block";
+         }
+      });
    });
 });
